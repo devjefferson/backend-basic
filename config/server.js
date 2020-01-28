f617452020e18bd97e4417c06e4a072d3f09909e
_devjefferson <drpcscan@gmail.com>
2020-01-28 16:26:30 -0200

    Install Morgan

--- config/server.js
+++ config/server.js
@@ -1,13 +1,13 @@
 const http = require('http')
-
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 const routes = require('../src/routes')
 const server = http.createServer(app);
 require("dotenv").config()
 
+app.use(morgan('dev'))
 app.use(express.json())
-
 app.use(routes)
 
 
